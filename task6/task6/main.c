6228c818b02a3e7bb3202cb3688a57f0973ca8f5
Anna P <annap@MacBook-Pro-Anna.local>
2017-04-10 02:43:16 +0300

    Task6 done. Program doesn’t work with several implementations of function ICPrint(). Result-> error : /Users/annap/ICTasks/task6/task6/main.c:28:6: Redefinition of 'ICPrint' Build succeeded with one implementation ICPrint()

---
+++ task6/task6/main.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  task6
+//
+//  Created by Anna P. on 10.04.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+
+//  6. Створити декілька імплементацій ICPrint та перевірити, чи програма працює, якщо не працює то закоментувати стільки імплементацій, щоб програма почала працювати;
+
+#include <stdio.h>
+
+void ICPrint();
+void ICPrint();
+void ICPrint();
+
+
+int main(int argc, const char * argv[]) {
+    ICPrint();
+    
+    return 0;
+}
+
+void ICPrint() {
+    printf("Hello, World!\n");
+}
+
+/*void ICPrint() {
+    printf("Hello, World!\n");
+}*/
+
+/*void ICPrint() {
+    printf("Hello, World!\n");
+}*/

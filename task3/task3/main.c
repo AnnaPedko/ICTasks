f4959cf1f0f55aa38f9fd04789c2397ac462d583
Anna P <annap@MacBook-Pro-Anna.local>
2017-04-10 02:14:31 +0300

    Task3 results : Program doesn’t compile with warning /Users/annap/ICTasks/task3/task3/main.c:14:5: Implicit declaration of function 'ICPrint' is invalid in C99
    and error
    /Users/annap/ICTasks/task3/task3/main.c:19:6: Conflicting types for 'ICPrint'

---
+++ task3/task3/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  task3
+//
+//  Created by Anna P. on 10.04.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+
+//3. Перенести імплементацію після main та перевірити, чи програма працює;
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    ICPrint();
+    
+    return 0;
+}
+
+void ICPrint() {
+    printf("Hello, World!\n");
+}

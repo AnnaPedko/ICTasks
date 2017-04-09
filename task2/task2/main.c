ef8c730401dfe7ef61c50a9f5dc6d567664c86ad
Anna P <annap@MacBook-Pro-Anna.local>
2017-04-10 01:58:37 +0300

    Task description added

--- task2/task2/main.c
+++ task2/task2/main.c
@@ -6,6 +6,9 @@
 //  Copyright © 2017 Anna P. All rights reserved.
 //
 
+//2. Винести виклик printf у окрему функцію ICPrint, яка б нічого не приймала та не повертала, а вже її викликати в main, при цьому хай її імплементація буде до main написана;
+
+
 #include <stdio.h>
 
 void ICPrint() {

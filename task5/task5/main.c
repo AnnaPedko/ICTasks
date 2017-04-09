d17a3b9c2cba5bf164d52095418ab418a4db6b68
Anna P <annap@MacBook-Pro-Anna.local>
2017-04-10 02:31:05 +0300

    task5 done. Build succeeded.

---
+++ task5/task5/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  task5
+//
+//  Created by Anna P. on 10.04.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+
+//5. Створити декілька декларацій ICPrint та перевірити, чи програма працює;
+
+#include <stdio.h>
+
+void ICPrint();
+void ICPrint();
+void ICPrint();
+
+
+int main(int argc, const char * argv[]) {
+    ICPrint();
+    
+    return 0;
+}
+
+void ICPrint() {
+    printf("Hello, World!\n");
+}

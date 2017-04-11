1b009b70d2392b7895cc3c7bc7476e553201a6b6
Anna P <annap@MacBook-Pro-Anna.local>
2017-04-11 08:02:56 +0300

    task11 done. Preprocess inserts declaration of functions from file ICPrinting.h three times

---
+++ task11/task11/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  task11
+//
+//  Created by Anna P. on 11.04.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+
+#include <stdio.h>
+#include "ICPrinting.h"
+#include "ICPrinting.h"
+#include "ICPrinting.h"
+
+int main(int argc, char **argv) {
+    ICPrint();
+    ICPrintParents();
+    
+    return 0;
+}

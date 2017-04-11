7848aa33e79c0eb17870f2a376e22dffa5487a9f
Anna P <annap@MacBook-Pro-Anna.local>
2017-04-11 07:17:11 +0300

    task 10 done. Build succeed

---
+++ task10/task10/main.c
@@ -0,0 +1,32 @@
+//
+//  main.c
+//  task10
+//
+//  Created by Anna P. on 11.04.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+
+//10. Створити нову функцію, ICPrintParents, яка б викликала ICPrint та сама виводила "Hello, papa!", викликати цю функцію з main, поексперіментувати зі взаєморозташування ICPrintParents відносно ICPrint та main, спробувати варіанти з використанням декларацій та без, розповісти, які варіанти розташування спробували та я кі з них спрацювали, а які - ні.
+
+#include <stdio.h>
+
+void ICPrint();
+void ICPrintParents();
+
+int main(int argc, const char * argv[]) {
+    ICPrintParents();
+    
+    return 0;
+}
+
+//ICPrint() writes the C string pointed by format to the standard output (stdout).
+
+void ICPrint() {
+    printf("Hello, mama!\n");
+}
+
+void ICPrintParents() {
+    ICPrint();
+    
+    printf("Hello, papa!\n");
+}

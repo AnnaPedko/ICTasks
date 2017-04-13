a8eab2641e35224bef150f0491eca6eb07d49cca
Anna P <annap@MacBook-Pro-Anna.local>
2017-04-13 20:15:14 +0300

    task35

---
+++ task35/task35/main.c
@@ -0,0 +1,56 @@
+//
+//  main.c
+//  task35
+//
+//  Created by Anna P. on 13.04.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+
+//1. Вывести размер типов через sizeof.
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Size of char = %lu\n",sizeof(char));
+    printf("Size of int = %lu\n",sizeof(int));
+    printf("Size of double = %lu\n",sizeof(double));
+    printf("Size of float = %lu\n",sizeof(double));
+    printf("Size of signed char = %lu\n",sizeof(signed char));
+    printf("Size of unsigned char = %lu\n",sizeof(unsigned char));
+    printf("Size of short = %lu\n",sizeof(short));
+    printf("Size of short int = %lu\n",sizeof(short int));
+    printf("Size of signed short int = %lu\n",sizeof(signed short int));
+    printf("Size of signed short = %lu\n",sizeof(signed short));
+    printf("Size of unsigned short = %lu\n",sizeof(unsigned short));
+    printf("Size of unsigned short int = %lu\n",sizeof(unsigned short int));
+    printf("Size of long = %lu\n",sizeof(long));
+    printf("Size of long int = %lu\n",sizeof(long int));
+    printf("Size of signed long = %lu\n",sizeof(signed long));
+    printf("Size of signed long int = %lu\n",sizeof(signed long int));
+    printf("Size of unsigned long = %lu\n",sizeof(unsigned long));
+    printf("Size of unsigned long int = %lu\n",sizeof(signed long int));
+    printf("Size of long long = %lu\n",sizeof(long long));
+    printf("Size of long long int= %lu\n",sizeof(long long int));
+    printf("Size of signed long long = %lu\n",sizeof(signed long long));
+    printf("Size of signed long long int = %lu\n",sizeof(signed long long int));
+    printf("Size of unsigned long long = %lu\n",sizeof(unsigned long long));
+    printf("Size of unsigned long long int = %lu\n",sizeof(unsigned long long int));
+    printf("Size of float = %lu\n",sizeof(float));
+    printf("Size of double = %lu\n",sizeof(double));
+    printf("Size of long double = %lu\n",sizeof(long double));
+
+    
+
+
+
+
+  
+    
+    
+
+    
+
+    
+    
+    return 0;
+}

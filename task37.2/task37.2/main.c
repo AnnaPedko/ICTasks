e3a396636cf460bacaaf7e9baab19ddfb99e855e
Anna P <annap@MacBook-Pro-Anna.local>
2017-04-14 00:45:07 +0300

    task37.2 done.

---
+++ task37.2/task37.2/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  task37.1
+//
+//  Created by Anna P. on 13.04.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+//2. создать тестовый метод, который бы с помощью offsetof выводил расположение каждого из элементов;
+
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "ICPrintSizeOf.h"
+
+int main(int argc, const char * argv[]) {
+    ICPrintSizeOf();
+    
+    return 0;
+}

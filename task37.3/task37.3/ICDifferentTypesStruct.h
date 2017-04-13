726dc352be0fe4ec36ced1b533e7869b1b6292c7
Anna P <annap@MacBook-Pro-Anna.local>
2017-04-14 01:14:53 +0300

    task37.3

---
+++ task37.3/task37.3/ICDifferentTypesStruct.h
@@ -0,0 +1,31 @@
+//
+//  ICDifferentTypesStruct.h
+//  task37.2
+//
+//  Created by Anna P. on 13.04.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+
+
+#ifndef ICDifferentTypesStruct_h
+#define ICDifferentTypesStruct_h
+#include <stdbool.h>
+
+typedef struct {
+    bool boolValueA;
+    bool boolValueB;
+    bool boolValueC;
+    bool boolValueD;
+    bool boolValueE;
+    bool boolValueF;
+    short shortValueA;
+    short shortValueB;
+    short shortValueC;
+    int intValue;
+    float floatValue;
+    double doubleValue;
+    long long longLongValue;
+    char *charPointer;
+} DifferentTypes ;
+
+#endif /* ICDifferentTypesStruct_h */
---
+++ task37.3/task37.3/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  task37.1
+//
+//  Created by Anna P. on 13.04.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+//3. перекомпоновать структуру так, чтобы она занимала наименьшее количество места;
+
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "ICPrintSizeOf.h"
+
+int main(int argc, const char * argv[]) {
+    ICPrintSizeOf();
+    
+    return 0;
+}

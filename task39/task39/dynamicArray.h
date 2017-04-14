1b5f22e948986a1fe97680e41cd5c82ec0913124
Anna P <annap@MacBook-Pro-Anna.local>
2017-04-15 00:42:33 +0300

    Works with int array

---
+++ task39/task39/dynamicArray.h
@@ -0,0 +1,14 @@
+//
+//  dynamicArray.h
+//  task39
+//
+//  Created by Anna P. on 14.04.17.
+//  Copyright © 2017 Anna P. All rights reserved.
+//
+
+#ifndef dynamicArray_h
+#define dynamicArray_h
+
+#include <stdio.h>
+
+#endif /* dynamicArray_h */
